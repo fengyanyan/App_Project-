bd3d2e870cca9bda9c7f274d7e33fbd434d7940a
fengyanyan <1154029883@qq.com>
2018-05-27 16:24:13 +0800

    添加描述

--- scripts/test_search.py
+++ scripts/test_search.py
@@ -4,5 +4,5 @@ class Test_001:
     @allure.step(title='测试步骤001')
     @pytest.mark.parametrize('a', [1, 2, 3])
     def test_hello(self, a):
-        # allure.attach('描述', '测试步骤001的描述')
+        allure.attach('描述', '测试步骤001的描述')
         assert a != 2
