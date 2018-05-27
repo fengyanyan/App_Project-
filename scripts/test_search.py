c48243c42eab6ebbb2ac8e7eca49db8172560feb
fengyanyan <1154029883@qq.com>
2018-05-27 16:20:14 +0800

    添加测试步骤

--- scripts/test_search.py
+++ scripts/test_search.py
@@ -1,7 +1,8 @@
-import pytest
+import pytest, allure
 
 class Test_001:
-
+    @allure.step(title='测试步骤001')
     @pytest.mark.parametrize('a', [1, 2, 3])
     def test_hello(self, a):
+        # allure.attach('描述', '测试步骤001的描述')
         assert a != 2
